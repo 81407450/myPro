df06331d8f802d81e260d53a279c4d0a95a882ee
lcs <xiaosheng0626@163.com>
2017-11-18 12:14:15 +0800

    add user

---
+++ src/cn/zbit/pojo/User.java
@@ -0,0 +1,33 @@
+package cn.zbit.pojo;
+
+public class User {
+	private Integer id;
+	private String userCode;
+	private String userName;
+	private String passWord;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+}
