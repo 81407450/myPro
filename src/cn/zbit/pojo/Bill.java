f58b10ecf1ee1b7a13998858b4d69660528dd12b
lcs <xiaosheng0626@163.com>
2017-11-18 12:20:08 +0800

    add bill

---
+++ src/cn/zbit/pojo/Bill.java
@@ -0,0 +1,13 @@
+package cn.zbit.pojo;
+
+public class Bill {
+	private Integer id;	//¶©µ¥±àºÅ
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
